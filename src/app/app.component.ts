daac0ba45a8607ad066e9f6a12fec4490b989b40
pedrodanieljardim <pedro.jardim@venhapranuvem.com.br>
2020-10-10 12:21:44 -0300

    form validations to fieldset

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,9 +10,17 @@ import { Todo } from 'src/models/todo.model';
 
 export class AppComponent {
   public todos : Todo[] = [];
-  public title :  String = "Minhas Tarefas";
+  public title :  String = "To do List!";
+  public form: FormGroup;
 
-  constructor() {
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
     this.todos.push(new Todo(1, 'Run!', false));
     this.todos.push(new Todo(2, 'Go to Gym!', false));
     this.todos.push(new Todo(3, 'Buy milk!', true));
