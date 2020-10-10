f5d4eb4f2f142d2fd123318f3de31a0ce82804ba
pedrodanieljardim <pedro.jardim@venhapranuvem.com.br>
2020-10-10 11:32:14 -0300

    add method to remove object toDo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,25 @@ export class AppComponent {
   constructor() {
     this.todos.push(new Todo(1, 'Run!', false));
     this.todos.push(new Todo(2, 'Go to Gym!', false));
-    this.todos.push(new Todo(3, 'Buy milk!', false));
+    this.todos.push(new Todo(3, 'Buy milk!', true));
   }
 
   alteraTexto(){
     this.title = 'Texto alterado!';
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(){
+
+  }
+  
+  markAsUndone(){
+
   }
 }
