70b0a39c56fd2f462eab191f60f8994e5b92e57a
pedrodanieljardim <pedro.jardim@venhapranuvem.com.br>
2020-10-10 11:18:29 -0300

    using construtor to instance new todo objets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'todo';
+  public todos : Todo[] = [];
+  public title :  String = "Minhas Tarefas";
+
+  constructor() {
+    this.todos.push(new Todo(1, 'Run!', false));
+    this.todos.push(new Todo(2, 'Go to Gym!', false));
+    this.todos.push(new Todo(3, 'Buy milk!', false));
+  }
+
+  alteraTexto(){
+    this.title = 'Texto alterado!';
+  }
 }
